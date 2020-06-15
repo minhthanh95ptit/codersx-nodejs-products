a9c5b23233f17ee4c42cd363e51ae4624f8fb560
minhthanh95ptit <minhthanh95ptit@gmail.com>
2020-06-15 10:30:00 +0700

    Video 2 - Template Engine

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ var app = express();
 
 var port = 3000;
 
-app.get('/', function (request, response) {
-  response.send('HAHAHA');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+  res.send("Hello codersx");
 })
 
+
+var todos = [
+  { id: 1, name: 'Đi Chợ' },
+  { id: 2, name: 'Nấu Cơm' },
+  { id: 3, name: 'Rửa bát' },
+  { id: 4, name: 'Học lập trình trên codersx' },
+  { id: 5, name: 'Tán gái' }
+];
+
 app.get('/todos', function (req, res) {
-  res.send('<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tạo codersx</li></ul>')
+  res.render('index', {
+    todos: todos
+  })
 })
 
 app.listen(3000, function () {
